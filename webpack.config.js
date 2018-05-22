b80dbccf03ae224be1ae34b29620adc2a72172ad
Marco Q <marcoq@gmail.com>
2018-05-22 11:33:57 +0200

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: { extensions: ['.js'] },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          },
+          {
+            test: /\.tpl$/,
+            use: 'underscore-template-loader',
+          },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: { hot: true },
+    devtool: 'source-map',
+}
