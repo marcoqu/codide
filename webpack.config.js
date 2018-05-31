94ed7427d1edebdb8660c5fb68dbc370295dd4be
Marco Q <marcoq@gmail.com>
2018-05-31 09:18:10 +0200

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,28 @@ module.exports = {
     entry: './src/entry.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     resolve: { extensions: ['.js'] },
     module: {
         rules: [
-          {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-          },
-          {
-            test: /\.tpl$/,
-            use: 'underscore-template-loader',
-          },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.tpl$/,
+                use: 'underscore-template-loader',
+            },
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/images/favicon.png'
+        }),
         new webpack.HotModuleReplacementPlugin(),
     ],
     devServer: { hot: true },
     devtool: 'source-map',
-}
+};
